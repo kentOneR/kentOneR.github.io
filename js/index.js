1cd38f29be3cc20cc5334b4b6cb0b3041a06a1bd
Quentin Blot <quentin.blot@seloger.com>
2021-12-28 17:01:50 +0100

    [HP] Init slick slider

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 (function () {
-    
+    var piloteSlider
+
+    $(document).ready(function(){
+        piloteSlider = $('.pilote__slider').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+    });
+
+    $('.pilote__nav .slider__dot').click(function() {
+        piloteSlider && piloteSlider.slick('slickGoTo', $(this).index())
+    })
+
+    var gameSlider
+
+    $(document).ready(function(){
+        gameSlider = $('.game__slider').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+    });
+
+    $('.game__nav .slider__dot').click(function() {
+        gameSlider && gameSlider.slick('slickGoTo', $(this).index())
+    })
+
 })()
